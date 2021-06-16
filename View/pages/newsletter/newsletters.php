a95032929839a251b5d27f597c1ef52794f6666f
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-06-16 14:18:44 +0200

    WIP newletters list

--- View/pages/newsletter/newsletters.php
+++ View/pages/newsletter/newsletters.php
@@ -1,12 +1,26 @@
 <div class="content d-flex flex-column w-100 p-4">
     <h2>Gestion des newsletters</h2>
     <div class="listNews">
-        <ul>
+        <table class="table table-light">
+            <thead>
+            <th>ID</th>
+            <th>Title</th>
+            <th></th>
+            <th></th>
+            </thead>
+
+            <tbody>
             <?php
             foreach ($_POST['news'] as $newsletter) {
-                echo "<li>$newsletter</li>";
+                echo "<tr>";
+                echo "<td>$newsletter->id</td>";
+                echo "<td>$newsletter->title</td>";
+                echo "<td><button class='btn'><i class='fas fa-pen-square'></i></button></td>";
+                echo "<td><button class='btn'><i class='fas fa-trash'></i></button></td>";
+                echo "</tr>";
             }
             ?>
-        </ul>
+            </tbody>
+        </table>
     </div>
 </div>
