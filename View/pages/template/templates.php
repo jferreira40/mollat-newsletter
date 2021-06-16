22dcb11181f5c630b0516ad2aea745a4ddd71b5d
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-06-16 14:49:48 +0200

    WIP templates list

--- View/pages/template/templates.php
+++ View/pages/template/templates.php
@@ -1,20 +1,20 @@
 <div class="content d-flex flex-column w-100 p-4">
-    <h2>Gestion des newsletters</h2>
+    <h2>Gestion des templates</h2>
     <div class="listNews">
         <table class="table table-light">
             <thead>
             <th>ID</th>
-            <th>Title</th>
+            <th>Name</th>
             <th></th>
             <th></th>
             </thead>
 
             <tbody>
             <?php
-            foreach ($_POST['news'] as $newsletter) {
+            foreach ($_POST['templates'] as $template) {
                 echo "<tr>";
-                echo "<td>$newsletter[id]</td>";
-                echo "<td>$newsletter[title]</td>";
+                echo "<td>$template[id]</td>";
+                echo "<td>$template[name]</td>";
                 echo "<td><button class='btn'><i class='fas fa-pen-square'></i></button></td>";
                 echo "<td><button class='btn'><i class='fas fa-trash'></i></button></td>";
                 echo "</tr>";
