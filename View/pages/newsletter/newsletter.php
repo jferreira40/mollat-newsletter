1d03fb3d317728105858cd8c368eebaafc470963
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-06-16 16:40:13 +0200

    Add back buttons

--- View/pages/newsletter/newsletter.php
+++ View/pages/newsletter/newsletter.php
@@ -1,4 +1,5 @@
 <div class="content d-flex flex-column w-100 p-4">
+    <a href="#" class="text-decoration-none mb-3"><i class="fas fa-arrow-left"></i> Retour</a>
     <h2>Éditer / Créer une nouvelle newsletter</h2>
     <div id="gjs">
         <?php echo $news['content'] ?>
--- View/pages/templates/template.php
+++ View/pages/templates/template.php
@@ -1,4 +1,5 @@
 <div class="content d-flex flex-column w-100 p-4">
+    <a href="#" class="text-decoration-none mb-3"><i class="fas fa-arrow-left"></i> Retour</a>
     <h2>Éditer / Créer un template de newsletter</h2>
     <div id="gjs">
         <?php echo $template['content'] ?>
