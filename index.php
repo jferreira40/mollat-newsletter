0c597f46ffbcf099b7273c39ea0c80bec22df3df
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-06-17 16:50:15 +0200

    edit edito

--- index.php
+++ index.php
@@ -83,6 +83,6 @@ if($params[0] != "") {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <script src="<?php echo ROOT ?>assets/grapesjs/editor.js"></script>
+    <script src="<?php echo ROOT ?>assets/grapesjs/editor.js" defer type="module"></script>
 </body>
 </html>
